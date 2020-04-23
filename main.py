25610ac990dd0d878e730c111560ae3a91085564
Yatish <tsh.bhardwaj725@gmail.com>
2020-04-24 01:32:07 +0530

    GUI elements added with logging and some bug fixes

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from tkinter import *
+import tkinter as tk
 import pcapy
+import logging
 
 root = Tk()
 root.geometry('720x480')
 root.title('Packet Sniffer')
+
+logging.basicConfig(filename="sniffer.log", format='%(asctime)s %(message)s', filemode='w')
+logger=logging.getLogger() 
+logger.setLevel(logging.DEBUG) 
 
 capture_devices = pcapy.findalldevs()
 
@@ -18,14 +24,23 @@ packet_size = 65536
 promiscous_mode = 1     # 0 for no/false and 1 for yes/true
 timeout = 0
 
+output = tk.Text(root)
+output.pack()
+
 def capture_packets():
-    cap = pcapy.open_live(capture_devices_list.get(),packet_size, promiscous_mode,timeout)
-    count = 1
-    while count<2:
-        (header,payload) = cap.next()
-        print(count + '\n' + header + '\n')
-        print(payload + '\n \n')
-        count+=1
+    try:
+        cap = pcapy.open_live(capture_devices_list.get(),packet_size, promiscous_mode,timeout)
+        count = 1
+        while count:
+            (header,payload) = cap.next()
+            output.insert(tk.END, header)
+            output.insert(tk.END, payload)
+            count+=1
+        root.update_idletasks()
+        logger.info('Packets captured successfully!')
+    except pcapy.PcapError:
+        logger.error('Problem with capture device!')
+        output.insert(tk.END,'\n Problem with capture device! \n')
 
 capture_packets_button = Button(root, text="Capture", command=capture_packets)
 capture_packets_button.pack()
